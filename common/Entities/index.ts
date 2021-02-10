604ceb9895074623a0705d90ab9e6d16ece01837
Donny Roufs <iamchets@yahoo.com>
2021-02-10 19:48:04 +0100

    export entities

---
+++ common/Entities/index.ts
@@ -0,0 +1,4 @@
+export * from "server/src/Entities/Order.entity";
+export * from "server/src/Entities/OrderToProduct.entity";
+export * from "server/src/Entities/User.entity";
+export * from "server/src/Entities/Product.entity";
